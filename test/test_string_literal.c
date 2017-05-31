7e4f21139dd2f7ba39b20a7c234bed32fb116846
QiqiDuan <2332917051@qq.com>
2017-05-31 21:00:54 +0800

    v00.00.000
    first commit

---
+++ test/test_string_literal.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main( void ) {
+    char *msg1;
+    msg1 = "string literal 1\n";
+
+    char *msg2 = "string literal 2\n";
+
+    printf("%s", msg1);
+    printf("%s", msg2);
+}
