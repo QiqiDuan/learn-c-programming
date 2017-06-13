6c23de2944a327d3d1df0bb44731b17835ba882b
QiqiDuan <2332917051@qq.com>
2017-06-13 21:17:54 +0800

    v00.00.001
    correct for UCHAR_MAX and USHRT_MAX

--- test/test_limits_h.c
+++ test/test_limits_h.c
@@ -8,11 +8,11 @@ int main( void ) {
     printf("\n");
     printf("SCHAR_MIN  : %d\n", SCHAR_MIN);
     printf("SCHAR_MAX  : %d\n", SCHAR_MAX);
-    printf("UCHAR_MAX  : %d\n", UCHAR_MAX);
+    printf("UCHAR_MAX  : %u\n", UCHAR_MAX);
     printf("\n");
     printf("SHRT_MIN   : %d\n", SHRT_MIN);
     printf("SHRT_MAX   : %d\n", SHRT_MAX);
-    printf("USHRT_MAX  : %d\n", USHRT_MAX);
+    printf("USHRT_MAX  : %u\n", USHRT_MAX);
     printf("\n");
     printf("INT_MIN    : %d\n", INT_MIN);
     printf("INT_MAX    : %d\n", INT_MAX);
