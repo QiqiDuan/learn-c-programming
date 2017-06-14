2cb51cfa82e604ffd75b0c8459e709843e572a0c
QiqiDuan <2332917051@qq.com>
2017-06-14 20:27:58 +0800

    v00.00.001
    add description

--- test/test_stdbool_h.c
+++ test/test_stdbool_h.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+/* 
+ * the header file <stdbool.h> is in
+ *     /usr/lib/gcc/x86_64-linux-gnu/5/include [for for Ubuntu 16.04.2 LTS]
+ */
+
 int main( void ) {
     _Bool false_0 = false;
     _Bool true_1 = true;
